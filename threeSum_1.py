32abfe248356be12ae41e94f526ed97bcee1f695
bxclib <1652256031@qq.com>
2019-02-10 09:16:15 +0100

    Create threeSum_1.py

---
+++ threeSum_1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def threeSum(self, nums: 'List[int]') -> 'List[List[int]]':
+        hashmap = {}
+        for idx,num in enumerate(nums):
+            if num not in hashmap:
+                hashmap[num] = 1
+            else:
+                hashmap[num] = hashmap[num] + 1
+        res = []
+        for idx,num1 in enumerate(nums):
+            hashmap_temp = hashmap.copy()
+            hashmap_temp = self.del_hash(hashmap_temp,num1)
+            for num2 in nums[idx+1:]:
+                hashmap_temp = self.del_hash(hashmap_temp,num2)
+                if -num1-num2 in hashmap_temp:
+                    res.append(tuple(sorted([num1,num2,-num1-num2])))
+        return list(set(res))
+                    
+    def del_hash(self,hashmap,num):
+        if hashmap[num] == 1:
+            del hashmap[num]
+        else:
+            hashmap[num] = hashmap[num] - 1
+        return hashmap
