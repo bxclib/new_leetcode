7d2a49f003ccc9598d16e4e5a7949665e4bb7a65
bxclib <1652256031@qq.com>
2019-02-10 06:53:42 +0100

    Create twoSum.py

---
+++ twoSum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: 'List[int]', target: 'int') -> 'List[int]':
+        hashmap = {}
+        for idx,num in enumerate(nums):
+            #print (hashmap)
+            if target - num in hashmap:
+                return [hashmap[target - num],idx]
+            else:
+                hashmap[num] = idx
